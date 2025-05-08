01cf8d73f98c3d7b7561b64ab1b1a235afed05ff
jeho <hobeats@gmail.com>
2025-05-08 12:13:11 +0900

    last-last-last

--- app/(tabs)/user/[username]/edit/page.tsx
+++ app/(tabs)/user/[username]/edit/page.tsx
@@ -1,13 +1,11 @@
 import db from "@/lib/db";
 import EditUserForm from "@/components/edit-user";
 
-type PageProps = {
-  params: {
-    username: string;
-  };
-};
-
-export default async function EditUser({ params }: PageProps) {
+export default async function EditUser({
+  params,
+}: {
+  params: { username: string };
+}) {
   const user = await db.user.findUnique({
     where: {
       username: params.username,
